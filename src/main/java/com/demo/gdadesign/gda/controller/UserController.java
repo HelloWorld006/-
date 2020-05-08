3b63c95301091292617edaabaa50676662758c86
junxinmo <820517963@qq.com>
2020-05-08 16:39:13 +0800

    登陆功能

--- src/main/java/com/demo/gdadesign/gda/controller/UserController.java
+++ src/main/java/com/demo/gdadesign/gda/controller/UserController.java
@@ -4,15 +4,18 @@ import com.alibaba.fastjson.JSONObject;
 import com.demo.gdadesign.gda.service.UserService;
 import com.demo.gdadesign.gda.untils.PubUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("login")
+    @PostMapping("/login")
     public JSONObject login(String username, String password) {
         String msg = userService.userLogin(username, password);
         if ("success".equals(msg)) {
